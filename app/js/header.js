6b399da0703efdc9eca625abc6bde894f1663090
Khamzat91 <khamzat91@mail.ru>
2022-12-20 22:34:10 +0300

    render selected in modal

--- app/js/header.js
+++ app/js/header.js
@@ -1,5 +1,9 @@
 let city = document.querySelector(".header__inner-top__city-location");
 let modal = document.querySelector(".header__inner-top__city-modal");
+const selectedAll = document.querySelector(
+  ".header__inner-top__city-modal__selected"
+);
+let inputModal = document.querySelector(".header__inner-top__city-modal__input");
 let clearInput = document.querySelector(
   ".header__inner-top__city-modal__clear"
 );
@@ -97,6 +101,15 @@ document.querySelector(".header__inner-top__city-modal__settlement").oninput =
         item.innerHTML = item.innerText;
       });
     }
+    if (inputCity.length) {
+    const imageInput = document.createElement('img');
+    imageInput.classList.add('header__inner-top__city-modal__close')
+    imageInput.src = "../images/close.svg"
+    inputModal.appendChild(imageInput)
+    }
+    imageInput.onclick = () => {
+      inputCity.value = ""
+    }
   };
 
 function inserMark(string, pos, len) {
@@ -109,42 +122,52 @@ function inserMark(string, pos, len) {
   );
 }
 
-//   <div class="header__inner-top__city-modal__item" data-type="country" id="all">
-// Московская область
-// </div>
+const onAddData = (e) => {
+  const selectCiti = document.createElement("div");
+  selectCiti.className ="header__inner-top__city-modal__select";
+  const imageCiti = document.createElement("img");
+  imageCiti.className = "header__inner-top__city-modal__select__clear";
+  imageCiti.src = "../images/close-modal.svg";
+  selectCiti.id = e.target.id;
+  selectCiti.setAttribute("data-type", e.target.getAttribute('data-type'));
+  e.target.getAttribute('data-type')
+  selectCiti.textContent = e.target.textContent;
+  selectCiti.appendChild(imageCiti);
+  selectedAll.appendChild(selectCiti);
+  imageCiti.onclick = () => {
+    selectCiti.remove()
+  }
+};
 
-const onAddData = () => {
-const selectCiti = document.createElement('.header__inner-top__city-modal__select')
-}
 
-
-
+let isModalOpen = false;
 const onShowModal = () => {
   modal.classList.toggle("toggle");
 
-  if (!modal.getAttribute("class").includes("toggle")) {
+  if (!modal.getAttribute("class").includes("toggle") && !isModalOpen) {
     for (let i = 0; i < ariasData.length; i++) {
       const area = document.createElement("div");
       area.className = "header__inner-top__city-modal__item";
       area.id = ariasData[i].id;
-      area.setAttribute("data-type", "country");
+      area.setAttribute("data-type", ariasData[i].type);
       area.textContent = ariasData[i].name;
       areasList.appendChild(area);
+      area.onclick = onAddData;
       if (ariasData[i].cities) {
         let cities = ariasData[i].cities;
         for (let j = 0; j < cities.length; j++) {
           const city = document.createElement("div");
           city.className = "header__inner-top__city-modal__item";
           city.id = cities[j].id;
-          city.setAttribute("data-type", "country");
+          city.setAttribute("data-type", cities[j].type);
           city.textContent = cities[j].name;
           areasList.appendChild(city);
+          city.onclick = onAddData;
         }
       }
     }
+    isModalOpen = true;
   }
 };
-
-
 
 city.onclick = onShowModal;
